80fa8719e5b5993707024fe5ce99b7a08d4f1e5f
camposCode <info@josecampos.io>
2022-01-11 16:11:52 +0100

    modelos y servicios de ingresos y egresos creados, servicios inyectados y formulas para calcular el total de ingresos y egresos asi como el porcentaje como el presupuesto total creadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CabeceroComponent } from './components/cabecero/cabecero.component';
+import { IngresoComponent } from './components/ingreso/ingreso.component';
+import { EgresoComponent } from './components/egreso/egreso.component';
+import { IngresoServicio } from './components/ingreso/ingreso.servicio';
+import { EgresoServicio } from './components/egreso/egreso.servicio';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceroComponent,
+    IngresoComponent,
+    EgresoComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    IngresoServicio,
+    EgresoServicio
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/egreso/egreso.servicio.ts
@@ -0,0 +1,7 @@
+import { Egreso } from './egreso.model';
+export class EgresoServicio{
+    egresos: Egreso[] = [
+        new Egreso("Alquiler", 419),
+        new Egreso("Seguro Coche", 89)
+    ]
+}
